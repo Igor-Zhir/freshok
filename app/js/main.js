3937b216575c4838480bdf3ae6988977a9ee3774
Igor-Zhr <iz09061991@gmail.com>
2022-10-27 15:13:21 +0300

    добавил filters на странице каталог

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,32 @@ $(function() {
 
   $('.menu__btn').on('click', function(){
       $('.menu__item-list').toggleClass('menu__item-list--active');
-  })
+  });
 
   
   $('.menu__btn').on('click', function(){
     $('.menu__btn').toggleClass('menu__btn--active');
-})
+});
+
+
+
+// Открыть корзину
+$('.user-nav__link--card').on('click', function(){
+  $('.card').toggleClass('card--active');
+});
+// Закрыть корзину
+$('.card__close-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.card').removeClass('card--active');
+});
+
+
+// При нажатии класс получает доп класс filters__btn-turn--active, стрелка меняет положение на 180 градусов
+$('.filters__btn-turn').on('click', function(){
+  $('.filters__btn-turn').toggleClass('filters__btn-turn--active');
+});
+
+
 
 $('.header-slider').slick({
   arrows: true,
