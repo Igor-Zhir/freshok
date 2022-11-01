312f579ed19aab4ab5b5172a83aba4abda0ec1d1
Igor-Zhr <iz09061991@gmail.com>
2022-11-01 11:27:29 +0200

    добавил ползунок, закончил страницу каталог

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ function scripts() {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'node_modules/nouislider/dist/nouislider.min.js',
         'app/js/main.js',
     ])
     .pipe(concat('main.min.js'))
